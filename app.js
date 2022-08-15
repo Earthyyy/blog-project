9d7f0a0796c6ea541dd37637269c74dbbcea5089
Earthyy <elardi2001@gmail.com>
2022-08-15 13:02:08 +0100

    add routes and test files

--- app.js
+++ app.js
@@ -2,7 +2,10 @@
 const express = require('express');
 const logger = require('morgan');
 const swaggerRouter = require('./routes/docs');
-
+const userRouter = require('./routes/users');
+const articleRouter = require('./routes/users');
+const commentRouter = require('./routes/users');
+const categoryRouter = require('./routes/users');
 
 
 
@@ -12,6 +15,12 @@ const app = express();
 app.use(express.json());
 app.use(logger('dev'));
 app.use("/docs",swaggerRouter);
+app.use('api/v1/users',userRouter);
+app.use('api/v1/articles',articleRouter);
+app.use('api/v1/comments',commentRouter);
+app.use('api/v1/categories',categoryRouter);
+
+
 
 app.get('/',(req,res) => {
     res.status(200).json({
---
+++ routes/users.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+
+
+
+
+// TODO: Implement CRUD operations
+router.get('/');
+router.get('/:id');
+router.post('/');
+router.put('/:id');
+router.delete('/:id');
+
+
+
+module.exports = router
